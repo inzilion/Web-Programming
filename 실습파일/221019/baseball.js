c8a8baecd6618e7940daac1ff69253f21a2366e5
inzilion <inzilion@gmail.com>
2022-10-20 15:29:16 +0900

    Update baseball.js

--- 실습파일/221019/baseball.js
+++ 실습파일/221019/baseball.js
@@ -17,30 +17,39 @@ const $btn = document.querySelector('button');
 const tries = [];
 
 $btn.addEventListener('click', () => {
-  const value = $input.value;  
+  const _value = $input.value;  
   $input.value = '';
   
-  if(value.length != 4)
+  if(_value.length != 4)
     return alert("4자리를 입력해라.");
-  if(new Set(value).size != 4)
+  if(new Set(_value).size != 4)
     return alert("중복있어.");
-  if(tries.includes(value))
+  if(tries.includes(_value))
     return alert("이미 입력한 값");
   
-  tries.push(value);
+  tries.push(_value);
   console.log(tries);
-  if(answer.join('') == value)
+  if(answer.join('') == _value)
     return $result.textContent = "홈런!!!";  
 
   if(tries.length == 10)
-    $result.textContent = "Game Over!!!";
-
+    return $result.textContent = `Game Over!!!  정답은 ${answer.join('')}`;
+    
+  
+  let strike = 0;
+  let ball = 0;
+  for(let i=0; i<answer.length; i++){
+    for(let j=0; j<_value.length; j++){
+      if(answer[i] != _value[j]) continue;
+      if(i == j) strike++;
+      else       ball++;
+    }
+  }
+  if(strike == 0 && ball == 0){
+    $result.append("아웃");
+  }
+  else  
+    $result.append(`${tries.length}차 시도 : ${_value} -> S : ${strike}, B : ${ball}`);
+  
+  $result.append(document.createElement('br'));
 })
-
-
-
-
-
-
-
-
